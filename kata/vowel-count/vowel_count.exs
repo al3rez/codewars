b5289dc021e0e609812651559938687f2dc0d019
Alireza Bashiri <azbshiri@gmail.com>
2018-04-02 04:04:12 +0430

    Fix vowel count solution
    How?
    String.splitter/2 doesn't work don't know why yet so used
    String.graphemes/1 instead.

--- kata/vowel-count/vowel_count.exs
+++ kata/vowel-count/vowel_count.exs
@@ -1,9 +1,10 @@
 defmodule VowelCount do
   def get_count(str) do
     vowels = ["a", "e", "i", "o", "u"]
-    String.splitter(str, "")
-    |> Enum.take(String.length(str))
-    |> Enum.filter(fn(c) -> Enum.member?(vowels, c) end)
-    |> Enum.count
+
+    str
+    |> String.graphemes()
+    |> Enum.filter(fn c -> Enum.member?(vowels, c) end)
+    |> Enum.count()
   end
 end
